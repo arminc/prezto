3c51a89b32fabe1066b6f22c992135b9b3acdd05
Armin <armin@coralic.nl>
2020-03-21 17:02:20 +0100

    changes

---
+++ custom/kubernetes/init.zsh
@@ -0,0 +1,25 @@
+# Return if requirements are not found.
+if (( ! ${+commands[kubectl]} )); then
+  return 1
+fi
+
+# Enable completion for 'kubectl'.
+cache_file="${0:h}/cache.zsh"
+kubectl_command="${commands[kubectl]}"
+
+if [[ "${kubectl_command}" -nt "${cache_file}" || ! -s "${cache_file}" ]]; then
+  ${kubectl_command} completion zsh >! "${cache_file}" 2> /dev/null
+fi
+
+source "${cache_file}"
+unset cache_file kubectl_command
+
+#
+# Aliases
+#
+
+alias k='kubectl'
+alias kx='kubectx'
+
+alias kg='kubectl get'
+alias kgp='kubectl get pods'
